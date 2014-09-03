17ec5f166e5ff0cc3603fb616a9ccb6079235d04
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 14:57:41 -0400

    even more debug
    look out line one and last line

--- movement.js
+++ movement.js
@@ -1,9 +1,6 @@
-var main = function() {
  $(document).ready(function() {
  $('.thumbnail').mouseenter(function() {
         $('.thumbnail').fadeTo('fast', .25);
     });
 });
-});
 
-$(document).ready(main);
