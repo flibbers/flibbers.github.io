8751a6c3a2962769ed5d06efb16321c72b267c54
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 15:46:09 -0400

    debug mouseout

--- movement.js
+++ movement.js
@@ -1,7 +1,6 @@
  $(document).ready(function() {
  $('.thumbnail1').mouseenter(function() {
         $('.thumbnail1').fadeTo('fast', .25);
-    });
  $('.thumbnail1').mouseleave(function() {
  		$('thumbnail1').fadeTo('fast', 1);
  	}); 
