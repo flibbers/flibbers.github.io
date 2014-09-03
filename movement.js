3071b550121bb6d76a8b610229b1f213cdc6dfec
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 18:46:07 -0400

    debug mouseleave
    trying another janky thing

--- movement.js
+++ movement.js
@@ -3,7 +3,7 @@
         $('.thumbnail1').replaceWith('<img src="https://38.media.tumblr.com/64f6dc7040d8fcfb8e40ebe831373db9/tumblr_nb5h6vWkap1r1pjv2o3_r2_400.jpg">');
     }); 
     $('.thumbnail1').mouseleave(function() {
-        $('.thumbnail1')
+        $('.thumbnail1').replaceWith('<img src="http://31.media.tumblr.com/8665c4aa2a6fecc5fb3b5aa36b6c464a/tumblr_n8q6an9ALt1r1pjv2o1_400.jpg">')
     });
     $('.thumbnail2').mouseenter(function() {
         $('.thumbnail2').replaceWith('<img src="http://31.media.tumblr.com/c31e225e6657f6e66ccb5f0f1202ae3d/tumblr_nb5h6vWkap1r1pjv2o2_400.jpg">');
