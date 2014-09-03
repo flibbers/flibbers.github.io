94fb7697a9d5e8d204c431858afcb6adab7041d3
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 15:04:11 -0400

    add fade out
    added a mouse leave handler to return thumbnail 1 to full opacity

--- movement.js
+++ movement.js
@@ -2,5 +2,8 @@
  $('.thumbnail1').mouseenter(function() {
         $('.thumbnail1').fadeTo('fast', .25);
     });
+ $('.thumbnail1').mouseleave(function() {
+ 		$('thumbnail1').fadeTo('fast', 1);
+ 	});
 });
 
