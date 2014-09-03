ebf39f15f69e93596158ff13616e2d22dc48deb2
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 15:09:12 -0400

    add fade to all thumbnails
    added fade to all thumbnails by copy pasting the code for the first
    thumbnail.  Still need to get the fadeTo full opacity to work.

--- movement.js
+++ movement.js
@@ -4,6 +4,36 @@
     });
  $('.thumbnail1').mouseleave(function() {
  		$('thumbnail1').fadeTo('fast', 1);
+ 	}); 
+ $('.thumbnail2').mouseenter(function() {
+        $('.thumbnail2').fadeTo('fast', .25);
+    });
+ $('.thumbnail2').mouseleave(function() {
+ 		$('thumbnail2').fadeTo('fast', 1);
+ 	}); 
+ $('.thumbnail3').mouseenter(function() {
+        $('.thumbnail3').fadeTo('fast', .25);
+    });
+ $('.thumbnail3').mouseleave(function() {
+ 		$('thumbnail3').fadeTo('fast', 1);
+ 	}); 
+ $('.thumbnail4').mouseenter(function() {
+        $('.thumbnail4').fadeTo('fast', .25);
+    });
+ $('.thumbnail4').mouseleave(function() {
+ 		$('thumbnail4').fadeTo('fast', 1);
+ 	});
+  $('.thumbnail5').mouseenter(function() {
+        $('.thumbnail5').fadeTo('fast', .25);
+    });
+ $('.thumbnail5').mouseleave(function() {
+ 		$('thumbnail5').fadeTo('fast', 1);
+ 	});
+  $('.thumbnail6').mouseenter(function() {
+        $('.thumbnail6').fadeTo('fast', .25);
+    });
+ $('.thumbnail6').mouseleave(function() {
+ 		$('thumbnail6').fadeTo('fast', 1);
  	});
 });
 
