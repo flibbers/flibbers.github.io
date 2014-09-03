8185ae5e7fca1132c80f290a38e679a022bfa571
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 15:52:30 -0400

    debug mouseleave
    took out the ); of the first set, to try and open up the whole
    function/include the mouse leave

--- movement.js
+++ movement.js
@@ -1,37 +1,37 @@
  $(document).ready(function() {
  $('.thumbnail1').mouseenter(function() {
         $('.thumbnail1').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail1').mouseleave(function() {
  		$('thumbnail1').fadeTo('fast', .5);
  	}); 
  $('.thumbnail2').mouseenter(function() {
         $('.thumbnail2').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail2').mouseleave(function() {
  		$('thumbnail2').fadeTo('fast', .5);
  	}); 
  $('.thumbnail3').mouseenter(function() {
         $('.thumbnail3').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail3').mouseleave(function() {
  		$('thumbnail3').fadeTo('fast', .5);
  	}); 
  $('.thumbnail4').mouseenter(function() {
         $('.thumbnail4').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail4').mouseleave(function() {
  		$('thumbnail4').fadeTo('fast', .5);
  	});
   $('.thumbnail5').mouseenter(function() {
         $('.thumbnail5').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail5').mouseleave(function() {
  		$('thumbnail5').fadeTo('fast', .5);
  	});
   $('.thumbnail6').mouseenter(function() {
         $('.thumbnail6').fadeTo('fast', .25);
-    });
+    }
  $('.thumbnail6').mouseleave(function() {
  		$('thumbnail6').fadeTo('fast', .5);
  	});
