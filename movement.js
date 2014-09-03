be2d2675c05f78dbab8a2ce29795bc99d48d41e8
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 16:20:15 -0400

    debug imagehover thumbnail1
    took out a .

--- movement.js
+++ movement.js
@@ -1,5 +1,5 @@
  $(document).ready(function() {
- 	 $('.thumbnail1').hover(function () {
+ 	 $('thumbnail1').hover(function () {
         this.src = 'https://31.media.tumblr.com/8665c4aa2a6fecc5fb3b5aa36b6c464a/tumblr_n8q6an9ALt1r1pjv2o1_400.jpg';
     }, function () {
         this.src = 'http://33.media.tumblr.com/4d7f8d9187416216230e4bc29e1a8def/tumblr_nb5h6vWkap1r1pjv2o3_250.jpg';
