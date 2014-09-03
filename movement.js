b8c417d3d963cf9bc661e0a7b0411277198f9e50
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-03 14:51:37 -0400

    changing jQuery thumbnail effect
    changed thumbnail effect to a fast fadeout

--- movement.js
+++ movement.js
@@ -1,9 +1,7 @@
 var main = function() {
-$("img").hover(function() {
-		$(this).attr("src","http://31.media.tumblr.com/76e42c52437ab9f99ed2d36646293d2f/tumblr_nb5h6vWkap1r1pjv2o1_400.jpg");
-			}, function() {
-		$(this).attr("src","https://31.media.tumblr.com/8665c4aa2a6fecc5fb3b5aa36b6c464a/tumblr_n8q6an9ALt1r1pjv2o1_400.jpg");
-	});
+ $('.thumbnail').mouseenter(function() {
+        $('.thumbnail').fadeTo('fast', 1);
+    });
 });
 
 $(document).ready(main);
