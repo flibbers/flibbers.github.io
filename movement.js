8e6836afd34b62bc7dc36d07a54aaa1dd2a3c32e
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-13 13:55:38 -0400

    debug buttons on homepage
    changed the buttons all to fadeOut, tried to get the fadeout/in to
    function on all of them

--- movement.js
+++ movement.js
@@ -3,25 +3,37 @@
         $('.thumbnail1').fadeOut('slow', .5);
     });
     $('.thumbnail1').mouseleave(function() {
-        $('.thumbnail').fadeOut('slow', 1);
-    });
-    $('.thumbnail1').mouseleave(function() {
-        $('.thumbnail1').replaceWith('<img src="http://31.media.tumblr.com/8665c4aa2a6fecc5fb3b5aa36b6c464a/tumblr_n8q6an9ALt1r1pjv2o1_400.jpg">')
+        $('.thumbnail1').fadeOut('slow', 1);
     });
     $('.thumbnail2').mouseenter(function() {
-        $('.thumbnail2').replaceWith('<img src="http://31.media.tumblr.com/c31e225e6657f6e66ccb5f0f1202ae3d/tumblr_nb5h6vWkap1r1pjv2o2_400.jpg">');
+        $('.thumbnail2').fadeOut('slow', .5);
     });
- $('.thumbnail3').mouseenter(function() {
-        $('.thumbnail3').replaceWith('<img src="http://31.media.tumblr.com/76e42c52437ab9f99ed2d36646293d2f/tumblr_nb5h6vWkap1r1pjv2o1_400.jpg">');
+    $('.thumbnail2').mouseleave(function() {
+        $('.thumbnail2').fadeOut('slow', 1);
     });
- $('.thumbnail4').mouseenter(function() {
-        $('.thumbnail4').replaceWith('<img src="http://38.media.tumblr.com/91e2a672df0fd8ab416d4e9577c3f1c4/tumblr_nb5h6vWkap1r1pjv2o4_400.jpg">');
+     $('.thumbnail3').mouseenter(function() {
+        $('.thumbnail3').fadeOut('slow', .5);
     });
- $('.thumbnail5').mouseenter(function() {
-        $('.thumbnail5').replaceWith('<img src="http://33.media.tumblr.com/8b18e139fe70e9be7b7bc0055a091f79/tumblr_nb5h6vWkap1r1pjv2o5_400.jpg">');
+    $('.thumbnail3').mouseleave(function() {
+        $('.thumbnail3').fadeOut('slow', 1);
     });
- $('.thumbnail6').mouseenter(function() {
-        $('.thumbnail6').replaceWith('<img src="http://33.media.tumblr.com/3bc5759a73bc70c2018f6097b2e7d66d/tumblr_nb5h6vWkap1r1pjv2o6_400.jpg">');
+     $('.thumbnail4').mouseenter(function() {
+        $('.thumbnail4').fadeOut('slow', .5);
+    });
+    $('.thumbnail4').mouseleave(function() {
+        $('.thumbnail4').fadeOut('slow', 1);
+    });
+    $('.thumbnail5').mouseenter(function() {
+        $('.thumbnail5').fadeOut('slow', .5);
+    });
+    $('.thumbnail5').mouseleave(function() {
+        $('.thumbnail5').fadeOut('slow', 1);
+    });
+    $('.thumbnail6').mouseenter(function() {
+        $('.thumbnail6').fadeOut('slow', .5);
+    });
+    $('.thumbnail6').mouseleave(function() {
+        $('.thumbnail6').fadeOut('slow', 1);
     });
 });
 
