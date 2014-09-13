ec485a722508743ac436d3fed72691ad21722c2d
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-13 18:17:22 -0400

    changed fadeTo speed, added image to jumbotron
    changed fadeTo speed from slow to fast; image added to css jumbotron

--- movement.js
+++ movement.js
@@ -1,39 +1,39 @@
  $(document).ready(function() {
     $('.thumbnail1').mouseenter(function() {
-        $('.thumbnail1').fadeTo('slow', .5);
+        $('.thumbnail1').fadeTo('fast', .5);
     });
     $('.thumbnail1').mouseleave(function() {
-        $('.thumbnail1').fadeTo('slow', 1);
+        $('.thumbnail1').fadeTo('fast', 1);
     });
     $('.thumbnail2').mouseenter(function() {
-        $('.thumbnail2').fadeTo('slow', .5);
+        $('.thumbnail2').fadeTo('fast', .5);
     });
     $('.thumbnail2').mouseleave(function() {
-        $('.thumbnail2').fadeTo('slow', 1);
+        $('.thumbnail2').fadeTo('fast', 1);
     });
      $('.thumbnail3').mouseenter(function() {
-        $('.thumbnail3').fadeTo('slow', .5);
+        $('.thumbnail3').fadeTo('fast', .5);
     });
     $('.thumbnail3').mouseleave(function() {
-        $('.thumbnail3').fadeTo('slow', 1);
+        $('.thumbnail3').fadeTo('fast', 1);
     });
      $('.thumbnail4').mouseenter(function() {
-        $('.thumbnail4').fadeTo('slow', .5);
+        $('.thumbnail4').fadeTo('fast', .5);
     });
     $('.thumbnail4').mouseleave(function() {
-        $('.thumbnail4').fadeTo('slow', 1);
+        $('.thumbnail4').fadeTo('fast', 1);
     });
     $('.thumbnail5').mouseenter(function() {
-        $('.thumbnail5').fadeTo('slow', .5);
+        $('.thumbnail5').fadeTo('fast', .5);
     });
     $('.thumbnail5').mouseleave(function() {
-        $('.thumbnail5').fadeTo('slow', 1);
+        $('.thumbnail5').fadeTo('fast', 1);
     });
     $('.thumbnail6').mouseenter(function() {
-        $('.thumbnail6').fadeTo('slow', .5);
+        $('.thumbnail6').fadeTo('fast', .5);
     });
     $('.thumbnail6').mouseleave(function() {
-        $('.thumbnail6').fadeTo('slow', 1);
+        $('.thumbnail6').fadeTo('fast', 1);
     });
 });
 
