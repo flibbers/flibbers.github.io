e5726df1849935ea5e66e50b61645cf56a49ab38
Autumn Von Plinsky <avonplinsky@gmail.com>
2014-09-13 13:58:15 -0400

    debug fadeOut/fadeTo
    changed all the movement fadeOuts to fadeTo

--- movement.js
+++ movement.js
@@ -1,39 +1,39 @@
  $(document).ready(function() {
     $('.thumbnail1').mouseenter(function() {
-        $('.thumbnail1').fadeOut('slow', .5);
+        $('.thumbnail1').fadeTo('slow', .5);
     });
     $('.thumbnail1').mouseleave(function() {
-        $('.thumbnail1').fadeOut('slow', 1);
+        $('.thumbnail1').fadeTo('slow', 1);
     });
     $('.thumbnail2').mouseenter(function() {
-        $('.thumbnail2').fadeOut('slow', .5);
+        $('.thumbnail2').fadeTo('slow', .5);
     });
     $('.thumbnail2').mouseleave(function() {
-        $('.thumbnail2').fadeOut('slow', 1);
+        $('.thumbnail2').fadeTo('slow', 1);
     });
      $('.thumbnail3').mouseenter(function() {
-        $('.thumbnail3').fadeOut('slow', .5);
+        $('.thumbnail3').fadeTo('slow', .5);
     });
     $('.thumbnail3').mouseleave(function() {
-        $('.thumbnail3').fadeOut('slow', 1);
+        $('.thumbnail3').fadeTo('slow', 1);
     });
      $('.thumbnail4').mouseenter(function() {
-        $('.thumbnail4').fadeOut('slow', .5);
+        $('.thumbnail4').fadeTo('slow', .5);
     });
     $('.thumbnail4').mouseleave(function() {
-        $('.thumbnail4').fadeOut('slow', 1);
+        $('.thumbnail4').fadeTo('slow', 1);
     });
     $('.thumbnail5').mouseenter(function() {
-        $('.thumbnail5').fadeOut('slow', .5);
+        $('.thumbnail5').fadeTo('slow', .5);
     });
     $('.thumbnail5').mouseleave(function() {
-        $('.thumbnail5').fadeOut('slow', 1);
+        $('.thumbnail5').fadeTo('slow', 1);
     });
     $('.thumbnail6').mouseenter(function() {
-        $('.thumbnail6').fadeOut('slow', .5);
+        $('.thumbnail6').fadeTo('slow', .5);
     });
     $('.thumbnail6').mouseleave(function() {
-        $('.thumbnail6').fadeOut('slow', 1);
+        $('.thumbnail6').fadeTo('slow', 1);
     });
 });
 
